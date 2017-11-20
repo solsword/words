f3e9e35466b6be00fc5ec61a6e63ed9fb84e381f
Peter Mawhorter <pmawhorter@gmail.com>
2017-11-20 07:37:08 -0500

    Got scrolling to work. It's beautiful! -Peter Mawhorter

--- js/words/words.js
+++ js/words/words.js
@@ -6,6 +6,12 @@ define(["./draw", "./grid"], function(draw, grid) {
   var VIEWPORT_SIZE = 800.0;
   var VIEWPORT_SCALE = 1.0;
 
+  var CURRENT_SWIPE = null;
+
+  // Mouse scroll correction factors:
+  var PIXELS_PER_LINE = 18;
+  var LINES_PER_PAGE = 40;
+
   function start_game() {
     // set up canvas context
     CANVAS = document.getElementById("canvas");
@@ -26,19 +32,53 @@ define(["./draw", "./grid"], function(draw, grid) {
     window.requestAnimationFrame(animate);
 
     // set up event handlers
-    /*
-    Event.observe(CANVAS, 'click', function(e) {
+    document.onmousedown = function (e) {
+      if (e.preventDefault) { e.preventDefault(); }
+      CURRENT_SWIPE = [];
       var vpos = [
-        Event.pointerX(e) - CTX.bounds.left,
-        Event.pointerY(e) - CTX.bounds.top
-      ]
+        e.pageX - CTX.bounds.left,
+        e.pageY - CTX.bounds.top
+      ];
       var wpos = draw.world_pos(CTX, vpos);
-      // TODO: HERE!
-      if (e.preventDefault) {
-        e.preventDefault();
+      var gpos = grid.grid_pos(wpos);
+      CURRENT_SWIPE.push(gpos);
+    }
+
+    document.onmouseup = function(e) {
+      if (e.preventDefault) { e.preventDefault(); }
+      if (CURRENT_SWIPE != null && CURRENT_SWIPE.length > 0) {
+        // A non-empty swipe motion.
+        glyphs = []
+        CURRENT_SWIPE.foreach(function(gp) {
+          glyphs.push(grid.tile_at(gp)["glyph"])
+        });
       }
-    });
-    */
+      // either way reset CURRENT_SWIPE
+      CURRENT_SWIPE = null;
+    }
+
+    document.onmousemove = function (e) {
+      if (e.preventDefault) { e.preventDefault(); }
+    }
+
+    document.onwheel = function (e) {
+      if (e.preventDefault) { e.preventDefault(); }
+      var unit = e.deltaMode;
+      var x = e.deltaX;
+      var y = e.deltaY;
+
+      // normalize units to pixels:
+      if (unit == 1) {
+        x *= PIXELS_PER_LINE;
+        y *= PIXELS_PER_LINE;
+      } else if (unit == 2) {
+        x *= PIXELS_PER_LINE * LINES_PER_PAGE;
+        y *= PIXELS_PER_LINE * LINES_PER_PAGE;
+      }
+
+      CTX.viewport_center[0] += x;
+      CTX.viewport_center[1] -= y;
+    }
   }
 
   function animate(now) {
