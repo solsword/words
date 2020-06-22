4427baa1dda93319023b9b4c495e2d9ce0782af1
avilakat <66647686+avilakat@users.noreply.github.com>
2020-06-22 10:19:35 -0500

    created quiz.js
    merged and moved

--- js/setup_quiz.js
+++ js/setup_quiz.js
@@ -1,16 +1,3 @@
-requirejs.config({
-  baseURL: "js/",
-});
+import * as quiz from "./words/quiz.js";
 
-requirejs(
-  ["words/words"],
-  function(words) {
-    words.setup_quiz()
-  }
-);
-
-function GetSelectedValue(){
-				var value_selected = document.getElementById("language");
-				var result = value_selected.options[value_selected.selectedIndex].value;
-
-			}
+  quiz.setup_quiz();
---
+++ quiz.js
@@ -0,0 +1,48 @@
+function eventually_process_word_list(element) {
+  var files = element.files;
+  if (files === null || files === undefined || files.length < 1) {
+    setTimeout(eventually_process_word_list, 50, element);
+  } else {
+    var first = files[0];
+    var firstname = first.name;
+    var fr = new FileReader();
+    fr.onload = function (e) {
+      var file_text = e.target.result;
+      handle_uploaded_word_list(firstname.split(".")[0], file_text);
+    }
+    fr.readAsText(first);
+  }
+}
+
+function handle_uploaded_word_list(fileName, text){
+  let words = text.split(/\s+/);
+  if(words[words.length-1] == ""){
+    words.pop();
+  }
+  if(words[0] == ""){
+    words.shift();
+  }
+
+  // console.log(words)
+
+  var value_selected = document.getElementById("language");
+  var result = value_selected.options[value_selected.selectedIndex].value;
+
+  let wordsURL = words.join(";");
+  wordsURL = escape(wordsURL);
+  let link = document.getElementById("quiz_link");
+  link.href = encodeURI("index.html#words=" + wordsURL + ",domain=" + result);
+  link.innerText = link.href;
+
+}
+
+export function setup_quiz() {
+  // Setup function for the domain builder.
+  var file_input = document.getElementById("words_list");
+
+  file_input.onmousedown = function () { this.setAttribute("value", ""); };
+  file_input.ontouchstart = file_input.onmousedown;
+  file_input.onchange = function () {
+    eventually_process_word_list(this);
+  }
+};
